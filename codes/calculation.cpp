fee34cd95fb525aefa36515e48b60c2e83e23dc2
Jason <168611138+Nail-it-Jason@users.noreply.github.com>
2024-08-04 20:50:49 +0800

    Add files via upload

--- codes/calculation.cpp
+++ codes/calculation.cpp
@@ -70,3 +70,106 @@ int count_reverse(std::vector<int> &A, int p, int r){
     }
     return 0;
 }
+
+// 找到和最大的子数组
+void find_max_crossing_array(std::vector<int> & a, int & sum_ans,
+                             int & p, int & q, int low, int high){
+    int mid = (high + low) / 2;
+    int left_sum = -INF;
+    int sum = 0;
+    for(int i = mid; i >= low; --i){
+        sum += a[i];
+        if(sum > left_sum){
+            left_sum = sum;
+            p = i;
+        }
+    }
+
+    int right_sum = -INF;
+    sum = 0;
+    for(int j = mid + 1; j <= high; ++j){
+        sum += a[j];
+        if(sum > right_sum){
+            right_sum = sum;
+            q = j;
+        }
+    }
+    sum_ans = right_sum + left_sum;
+}
+
+void find_max_subarray(std::vector<int> & a, int & sum_ans,
+                       int & p, int & q, int low, int high){
+    if(low == high){
+        sum_ans = a[low];
+        p = low;
+        q = high;
+    }
+    else{
+        int mid = (high + low) / 2;
+        int left_low, left_high, left_sum,
+        right_low, right_high, right_sum,
+        cross_low, cross_high, cross_sum;
+        find_max_crossing_array(a, cross_sum, cross_low, cross_high, low, high);
+        find_max_subarray(a, left_sum, left_low, left_high, low, mid);
+        find_max_subarray(a, right_sum, right_low, right_high, mid + 1, high);
+        if(left_sum >= right_sum && left_sum >= cross_sum){
+            sum_ans = left_sum;
+            p = left_low;
+            q = left_high;
+        }
+        else if(right_sum >= left_sum && right_sum >= cross_sum){
+            sum_ans = right_sum;
+            p = right_low;
+            q = right_high;
+        }
+        else{
+            sum_ans = cross_sum;
+            p = cross_low;
+            q = cross_high;
+        }
+    }
+}
+
+// 找和最大的子数组，但是暴力求解，来自习题4.1-2
+void find_max_subarray_v(std::vector<int> & a, int & sum_ans, int & p, int & q){
+    int sum = 0, max_sum = -INF, all_max_sum = -INF, temp = 0;
+    for(int i = 0; i < a.size(); ++i){
+        sum = 0;
+        for(int j = i; j < a.size(); ++j){
+            sum += a[j];
+            if(sum > max_sum){
+                max_sum = sum;
+                temp = j;
+            }
+        }
+        if(max_sum > all_max_sum){
+            all_max_sum = max_sum;
+            p = i;
+            q = temp;
+        }
+    }
+    sum_ans = all_max_sum;
+}
+
+//非递归的线性算法
+void find_max_subarray_i(std::vector<int> & a, int & sum_ans, int & p, int & q) {
+    int max_sum = -INF, sum = -INF, currentHigh = 0, currentLow = 0,
+    High = 0, Low = 0;
+    for(int j = 0; j < a.size(); ++j){
+        currentHigh = j;
+        if(sum > 0)
+            sum += a[j];
+        else{
+            currentLow = j;
+            sum = a[j];
+        }
+        if(sum > max_sum){
+            max_sum = sum;
+            Low = currentLow;
+            High = currentHigh;
+        }
+    }
+    sum_ans = max_sum;
+    p = Low;
+    q = High;
+}
--- codes/calculation.h
+++ codes/calculation.h
@@ -8,4 +8,10 @@ int horner(std::vector<int> &A, int x);
 int naive_poly(std::vector<int> &A, int x);
 int count_two_arrays(std::vector<int> &A, int p, int q, int r);
 int count_reverse(std::vector<int> &A, int p, int r);
+void find_max_subarray(std::vector<int> & a, int & sum_ans,
+                       int & p, int & q, int low, int high);
+void find_max_crossing_array(std::vector<int> & a, int & sum_ans,
+                             int & p, int & q, int low, int high);
+void find_max_subarray_v(std::vector<int> & a, int & sum_ans, int & p, int & q);
+void find_max_subarray_i(std::vector<int> & a, int & sum_ans, int & p, int & q);
 #endif
